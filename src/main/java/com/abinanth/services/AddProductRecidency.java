bf7b485f71e6898fdb39cb682a9269c598b757fa
Abinanth <abinanthb1998@gmail.com>
2021-05-14 20:00:46 +0530

    Updated Sonar Issues

--- src/main/java/com/abinanth/services/AddProductRecidency.java
+++ src/main/java/com/abinanth/services/AddProductRecidency.java
@@ -16,27 +16,23 @@ public class AddProductRecidency {
 		return recidencyType;
 
 	}
-/**
- * // adding recidency in lowercase
- * // checking wheather the recidecy value contain in arraylist
- * // if new value string is validated
- * @param newRecidency
- * @return
- */
-	public static boolean addRecidencyType(String newRecidency) {
-		boolean valid = true;
 
-		newRecidency = newRecidency.toLowerCase(); 
-	
-		
-		if (recidencyType.contains(newRecidency)) {
-			
-			valid = false;
-		} else {
-			
-			if (RecidencyStringValidator.stringValidation(newRecidency)) {
+	/**
+	 * // adding recidency in lowercase // checking wheather the recidecy value
+	 * contain in arraylist // if new value string is validated
+	 * 
+	 * @param newRecidency
+	 * @return
+	 */
+	public static boolean addRecidencyType(String newRecidency) {
+		boolean valid = false;
+		if (RecidencyStringValidator.stringValidation(newRecidency)) {
+			newRecidency = newRecidency.toLowerCase();
+			if (recidencyType.contains(newRecidency)) {
+				valid = false;
+			} else {
 				recidencyType.add(newRecidency);
-			
+				valid = true;
 			}
 		}
 
--- src/test/java/com/abinanth/service/AddProductRecidencyTest.java
+++ src/test/java/com/abinanth/service/AddProductRecidencyTest.java
@@ -5,53 +5,53 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import com.abinanth.services.AddProductRecidency;
+
 public class AddProductRecidencyTest {
 
-	
-	
-		@Test
-		public void addProduct(){
-			String newRecidency="Textile";
-			
-	boolean newProductAdded=AddProductRecidency.addRecidencyType(newRecidency);
+	@Test
+	public void addProduct() {
+		String newRecidency = "Textile";
 
-	//System.out.println(recidencyType);
-			assertTrue(newProductAdded);
-			
-	}
-		@Test
-		public void addProductExisting(){
-			String newRecidency="Industry";
-			
-	boolean existingProductAdded=AddProductRecidency.addRecidencyType(newRecidency);
-			assertTrue(existingProductAdded);
-			
-	}
-		@Test
-		public void addProductExistingNull(){
-			String newRecidency=" ";
-			
-	boolean existingProductAdded=AddProductRecidency.addRecidencyType(newRecidency);
-			assertFalse(existingProductAdded);
-			
-	}
-		@Test
-		public void addProductExistingWithNumbers(){
-			String newRecidency="ind22ustry5644 ";
-			
-	boolean existingProductAdded=AddProductRecidency.addRecidencyType(newRecidency);
-			assertFalse(existingProductAdded);
-			
-	}
-		@Test
-		public void addProductExistingWithCapital(){
-			String newRecidency="INDUSRTY";
-			
-	boolean existingProductAdded=AddProductRecidency.addRecidencyType(newRecidency);
-			assertTrue(existingProductAdded);
-			
-	}
+		boolean newProductAdded = AddProductRecidency.addRecidencyType(newRecidency);
+
+		// System.out.println(recidencyType);
+		assertTrue(newProductAdded);
+
 	}
 
+	@Test
+	public void addProductExisting() {
+		String newRecidency = "Industry";
 
+		boolean existingProductAdded = AddProductRecidency.addRecidencyType(newRecidency);
+		assertTrue(existingProductAdded);
 
+	}
+
+	@Test
+	public void addProductExistingNull() {
+		String newRecidency = " ";
+
+		boolean existingProductAdded = AddProductRecidency.addRecidencyType(newRecidency);
+		assertFalse(existingProductAdded);
+
+	}
+
+	@Test
+	public void addProductExistingWithNumbers() {
+		String newRecidency = "ind22ustry5644 ";
+
+		boolean existingProductAdded = AddProductRecidency.addRecidencyType(newRecidency);
+		assertFalse(existingProductAdded);
+
+	}
+
+	@Test
+	public void addProductExistingWithCapital() {
+		String newRecidency = "INDUSRTY";
+
+		boolean existingProductAdded = AddProductRecidency.addRecidencyType(newRecidency);
+		assertTrue(existingProductAdded);
+
+	}
+}
